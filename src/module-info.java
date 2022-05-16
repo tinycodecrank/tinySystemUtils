cb2e98ec934fd96b9e571cb3f3be0f49ed9b196b
tinycodecrank <tinycodecrank@gmail.com>
2022-05-16 12:06:44 +0200

    update to jdk 17

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+module System
+{
+	exports de.tinycodecrank.autostart;
+	exports de.tinycodecrank.os;
+	exports de.tinycodecrank.io;
+	exports de.tinycodecrank.shortcut;
+
+	requires Collections;
+	requires Concurrent;
+	requires transitive Functional;
+	requires Monads;
+}
